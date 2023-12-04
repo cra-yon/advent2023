9b91e5f9e60461cb12d23d981d6f919a0e3164cb
Ryan Corrao <ryan@corrao.xyz>
2023-12-04 02:59:34 -0500

    renaming

--- day3/d3_p1.py
+++ day3/d3_p1.py
@@ -4,19 +4,19 @@ file.close()
 
 charlistlist, numbercoords, adjsymbolcoords = [], [], []
 
-def generatematrix(lines):
+def init_matrix(lines):
     for line in lines:
         charlistlist.append(list(line))
 
-def getsymbolcoords(matrix):
+def get_symbol_coords(matrix):
     coordlist = []
     for x, xitem in enumerate(matrix):
         for y, yitem in enumerate(matrix[x]):
             if not yitem.isnumeric() and not yitem == '.':
-                coordlist.extend((x + xc, y + yc) for xc in range(-1, 2) for yc in range(-1, 2) if validcoord(x + xc, y + yc))
+                coordlist.extend((x + xc, y + yc) for xc in range(-1, 2) for yc in range(-1, 2) if is_valid_coord(x + xc, y + yc))
     return coordlist
 
-def generatenumbers(matrix):
+def get_numbers(matrix):
     numlist = []
     for x, xitem in enumerate(matrix):
         tempnum, coordlist = '', []
@@ -31,23 +31,23 @@ def generatenumbers(matrix):
             numlist.append((tempnum, coordlist))
     return numlist
             
-def isadjsymbolcoord(x, y):
+def is_adj_symbol_coord(x, y):
     return (x, y) in adjsymbolcoords
 
 #this checks if a coordinate is in the bounds of the matrix
-def validcoord(x ,y):
+def is_valid_coord(x ,y):
     return x <= len(charlistlist) and y <= len(charlistlist[0])
 
 
-generatematrix(data)
-numbercoords = generatenumbers(charlistlist)
-adjsymbolcoords = getsymbolcoords(charlistlist)
+init_matrix(data)
+numbercoords = get_numbers(charlistlist)
+adjsymbolcoords = get_symbol_coords(charlistlist)
 
 numbersadjsymbols = []
 
 for number in numbercoords:
     for coord in number[1]:
-        if isadjsymbolcoord(coord[0], coord[1]):
+        if is_adj_symbol_coord(coord[0], coord[1]):
             numbersadjsymbols.append(int(number[0]))
             break
 
