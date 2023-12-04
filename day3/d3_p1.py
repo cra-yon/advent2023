6754f4fd469e60a026f96c6f06d15189ccb8bd20
Ryan Corrao <ryan@corrao.xyz>
2023-12-03 21:46:40 -0500

    added some clarification

--- day3/d3_p1.py
+++ day3/d3_p1.py
@@ -6,10 +6,12 @@ charlistlist = []
 numbercoords = []
 adjsymbolcoords = []
 
+#putting all characters from file into a matrix
 def generatematrix(lines):
     for line in lines:
         charlistlist.append(list(line))
 
+#this returns a list of {x,y} pairs for any instance of a number
 def getnumcoords(matrix):
     coordlist = []
     for x, xitem in enumerate(matrix):
@@ -18,19 +20,32 @@ def getnumcoords(matrix):
                 coordlist.append({x, y})
     return coordlist
 
-
+#this returns a list of {x,y} pairs for any valid adjacent symbol coordinate
 def getsymbolcoords(matrix):
     coordlist = []
     for x, xitem in enumerate(matrix):
         for y, yitem in enumerate(matrix[x]):
+            #if char is not a number or a '.' 
             if not yitem.isnumeric() and not yitem == '.':
+                # get all x-1 x x+1, y-1 y y+1 coordinate
                 for xc in range(-1,2):
                     for yc in range(-1,2):
                         if(validcoord(x + xc, y + yc)):
                             coordlist.append({x + xc, y + yc})
     return coordlist
 
-# def generatemumbers(list):
+#this is not complete
+def generatenumbers(matrix):
+    for x, xitem in enumerate(matrix):
+        tempnum = ''
+        isvalid = False
+        for y, yitem in enumerate(matrix):
+            if yitem.isnumeric():
+                tempnum = tempnum + yitem
+            
+def isadjsymbolcoord(x, y):
+    return 
+
 
 # def generatesum(list):
 
@@ -40,10 +55,10 @@ def validcoord(x ,y):
 
 generatematrix(data)
 
-print(charlistlist)
+#print(charlistlist)
 
 numbercoords = getnumcoords(charlistlist)
 adjsymbolcoords = getsymbolcoords(charlistlist)
 
-print(numbercoords)
+#print(numbercoords)
 print(adjsymbolcoords)
